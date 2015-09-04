Fixed eps in the calculation for the E field when th_0 != 90

lifetmm/examples.py
@@ -21,24 +21,25 @@ degree = pi / 180
 lambda_list = [1550] # in nm
 lambda_vac = 1550
 # incoming light angle
-th_0 = linspace(0, 90, num=90+1) # in degrees (convert in function argument)
+th_0 = linspace(0, 90, num=90+1, endpoint=False) # in degrees (convert in function argument)
 
 # ------------- DO CALCULATIONS  -----------------
-# list of layer thicknesses in nm
-d_list = [0, 1000, 1000, 0]
+# list of layer thicknesses in nm. First and last layer are semi-infinite ambient and substrate layers
+d_list = [inf, 10000, 10000, inf]
 # list of refractive indices
-n_list_med = [1, 1.5, 3, 3]
-n_list_bulk = [1, 1.5, 1.5, 1.5]
+n_list = [1, 1.5, 3, 3]
 
-# for th in th_0:
-data = TransferMatrix(d_list, n_list_med, lambda_vac, 90 * degree, 's')['E_square']
-    # print(th)
+data = np.array([0.]*sum(d_list[1:-1]))
+for th in th_0:
+    data += TransferMatrix(d_list, n_list, lambda_vac, th * degree, 's')['E_square']
 
 plt.figure()
 plt.plot(data)
 plt.xlabel('Position in Device (nm)')
 plt.ylabel('Normalized |E|$^2$Intensity')
 plt.title('E-Field Intensity in Device')
+plt.show()
+
 
 def sample1():
     """

lifetmm/lifetmm_core.py
@@ -14,7 +14,6 @@ from scipy.interpolate import interp1d
 # matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-
 def q_j(nj, n0, th_0):
     """
     Calculate the q factor for layer j
@@ -37,28 +36,30 @@ def I_mat(n1, n2, n0, pol, th_0):
     :param th_0: Angle of incidence of light (0 for normal, pi/2 for glancing)
     :return: I-matrix
     """
-    # transfer matrix at an interface
-    q1 = q_j(n1, n0, th_0)
-    q2 = q_j(n2, n0, th_0)
 
     if th_0 == 0:  # no difference between polarizations for normal incidence
         r = (n1 - n2) / (n1 + n2)
         t = (2 * n1) / (n1 + n2)
 
-    elif pol == 's':
-        r = (q1 - q2) / (q1 + q2)
-        t = (2 * q1) / (q1 + q2)
-
-    elif pol == 'p':
-        r = (q1 * n2**2 - q2 * n1**2) / (q1 * n2**2 + q2 * n1**2)
-        t = (2 * n1 * n2 * q1) / (q1 * n2**2 + q2 * n1**2)
-
     else:
-        raise ValueError("Polarisation must be 's' or 'p'")
+        # transfer matrix at an interface
+        q1 = q_j(n1, n0, th_0)
+        q2 = q_j(n2, n0, th_0)
+
+        if pol == 's':
+            r = (q1 - q2) / (q1 + q2)
+            t = (2 * q1) / (q1 + q2)
+
+        elif pol == 'p':
+            r = (q1 * n2**2 - q2 * n1**2) / (q1 * n2**2 + q2 * n1**2)
+            t = (2 * n1 * n2 * q1) / (q1 * n2**2 + q2 * n1**2)
+
+        else:
+            raise ValueError("Polarisation must be 's' or 'p' when angle of incidence is"
+                             " not 90$\degree$s")
 
     if t == 0:
-        raise ValueError('Transmission is zero.')
-        return 0
+        raise ValueError('Transmission is zero. Check input parameters.')
 
     return (1/t) * np.array([[1, r], [r, 1]], dtype=complex)
 
@@ -77,6 +78,7 @@ def L_mat(n, d, lam_vac, n0, th_0):
     return np.array([[np.exp(complex(0, -1.0 * eps * d)), 0],
                      [0, np.exp(complex(0, eps * d))]])
 
+
 def TransferMatrix(d_list, n_list, lam_vac, th_0, pol, x_step=1, reverse=False, glass=False):
     """
     Evaluate the transfer matrix over the entire structure.
@@ -85,6 +87,8 @@ def TransferMatrix(d_list, n_list, lam_vac, th_0, pol, x_step=1, reverse=False, 
     :param d_list: list of thicknesses for each layer
     :param th_0: angle of incidence (0 for normal, pi/2 for glancing)
     :param lam_vac: vacuum wavelength of light
+    :param glass: Bool. If there is a thick superstrate present then make true as interference
+                        in this layer is negligible
     :return: Dictionary of all input and output params related to structure
     """
     # convert lists to numpy arrays if they're not already.
@@ -98,6 +102,8 @@ def TransferMatrix(d_list, n_list, lam_vac, th_0, pol, x_step=1, reverse=False, 
                          'calculation at a time (1 wavelength, 1 angle, etc.)')
     if (n_list.ndim != 1) or (d_list.ndim != 1) or (n_list.size != d_list.size):
         raise ValueError("Problem with n_list or d_list!")
+    if (d_list[0] != inf) or (d_list[-1] != inf):
+        raise ValueError('d_list must start and end with inf!')
     if type(x_step) != int:
         raise ValueError('x_step must be an integer otherwise. Reduce SI unit'
                          'inputs for thicknesses and wavelengths for greater resolution ')
@@ -105,17 +111,18 @@ def TransferMatrix(d_list, n_list, lam_vac, th_0, pol, x_step=1, reverse=False, 
         raise ValueError('The light is not incident on the structure. Check input theta '
                          '(0 <= theta < 90')
 
-    # Flip structure if the optional argument 'reverse' is true
+    # Flip structure if the optional argument 'reverse' is True
     if reverse:
         d_list = d_list[::-1]
         n_list = n_list[::-1]
 
+    # Set first and last layer thicknesses to zero (from inf) - helps for summing later in program
+    d_list[0] = 0
+    d_list[-1] = 0
+
     num_layers = d_list.size
     n = n_list
-    n0 = n[0]
 
-    # calculate transfer marices, and field at each wavelength and position
-    d_list[0] = 0               # Thickness of layer light is originating from not important
     d_cumsum = np.cumsum(d_list)                              # Start position of each layer
     x_pos = np.arange((x_step / 2.0), sum(d_list), x_step)    # x positions to evaluate E field at
 
@@ -126,18 +133,17 @@ def TransferMatrix(d_list, n_list, lam_vac, th_0, pol, x_step=1, reverse=False, 
     x_mat = sum(comp1 > comp2, 0)  # TODO might need to get changed to better match python indices - check
 
     # calculate the total system transfer matrix S
-    S = I_mat(n[0], n[1], n0, pol, th_0)
-
+    S = I_mat(n[0], n[1], n[0], pol, th_0)
     for layer in range(1, num_layers - 1):
-        mL = L_mat(n[layer], d_list[layer], lam_vac, n0, th_0)
-        mI = I_mat(n[layer], n[layer + 1], n0, pol, th_0)
+        mL = L_mat(n[layer], d_list[layer], lam_vac, n[0], th_0)
+        mI = I_mat(n[layer], n[layer + 1], n[0], pol, th_0)
         S = np.asarray(np.mat(S) * np.mat(mL) * np.mat(mI))
 
-    # JAP Vol 86 p.487 Eq 9: Power Reflection
+    # JAP Vol 86 p.487 Eq 9 and 10: Power Reflection and Transmission
     R = abs(S[1, 0] / S[0, 0]) ** 2
     T = abs(1 / S[0, 0]) ** 2  # note this is incorrect https://en.wikipedia.org/wiki/Fresnel_equations
 
-    # calculate incoherent power transmission through thick superstrate to coherent layers (air - glass)
+    # Calculate incoherent power transmission into the glass - if glass is the ambient medium input
     # See Griffiths "Intro to Electrodynamics 3rd Ed. Eq. 9.86 & 9.87
     # If first layer in n_list is air (n=1) then these don't do anything
     if glass and not reverse:
@@ -154,29 +160,29 @@ def TransferMatrix(d_list, n_list, lam_vac, th_0, pol, x_step=1, reverse=False, 
     E = np.zeros(len(x_pos), dtype=complex)  # Initialise E field
     E_avg = np.zeros(num_layers)
     for layer in range(1, num_layers):
-        xi = 2 * np.pi * n[layer] / lam_vac
+        eps = (2 * np.pi * q_j(n[layer], n[0], th_0)) / lam_vac
         dj = d_list[layer]
         x_indices = np.nonzero(x_mat == layer)
         x = x_pos[x_indices] - d_cumsum[layer - 1]
         # Calculate S_Prime
-        S_prime = I_mat(n[0], n[1], n0, pol, th_0)
+        S_prime = I_mat(n[0], n[1], n[0], pol, th_0)
         for layerind in range(2, layer + 1):
-            mL = L_mat(n[layerind - 1], d_list[layerind - 1], lam_vac, n0, th_0)
-            mI = I_mat(n[layerind - 1], n[layerind], n0, pol, th_0)
+            mL = L_mat(n[layerind - 1], d_list[layerind - 1], lam_vac, n[0], th_0)
+            mI = I_mat(n[layerind - 1], n[layerind], n[0], pol, th_0)
             S_prime = np.asarray(np.mat(S_prime) * np.mat(mL) * np.mat(mI))
 
         # Calculate S_dprime (double prime)
         S_dprime = np.eye(2)
         for layerind in range(layer, num_layers - 1):
-            mI = I_mat(n[layerind], n[layerind + 1], n0, pol, th_0)
-            mL = L_mat(n[layerind + 1], d_list[layerind + 1], lam_vac, n0, th_0)
+            mI = I_mat(n[layerind], n[layerind + 1], n[0], pol, th_0)
+            mL = L_mat(n[layerind + 1], d_list[layerind + 1], lam_vac, n[0], th_0)
             S_dprime = np.asarray(np.mat(S_dprime) * np.mat(mI) * np.mat(mL))
 
         # Electric Field Profile
-        num = T * (S_dprime[0, 0] * np.exp(complex(0, -1.0) * xi * (dj - x)) + S_dprime[1, 0] * np.exp(
-            complex(0, 1) * xi * (dj - x)))
-        den = S_prime[0, 0] * S_dprime[0, 0] * np.exp(complex(0, -1.0) * xi * dj) + S_prime[0, 1] * S_dprime[
-            1, 0] * np.exp(complex(0, 1) * xi * dj)
+        num = T * (S_dprime[0, 0] * np.exp(complex(0, -1.0) * eps * (dj - x)) + S_dprime[1, 0] * np.exp(
+            complex(0, 1) * eps * (dj - x)))
+        den = S_prime[0, 0] * S_dprime[0, 0] * np.exp(complex(0, -1.0) * eps * dj) + S_prime[0, 1] * S_dprime[
+            1, 0] * np.exp(complex(0, 1) * eps * dj)
         E[x_indices] = num / den
 
         # TODO change the following divide