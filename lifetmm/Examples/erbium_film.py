4aa86b482e364a6358d5671f13951c49333fbe02
mn14tm <mn14tm@leeds.ac.uk>
2016-10-13 14:18:23 +0100

    blenheim

--- lifetmm/Examples/erbium_film.py
+++ lifetmm/Examples/erbium_film.py
@@ -1,58 +1,104 @@
 """
-Script to recreate the plots in the paper
-
-    'Quantum theory of spontaneous emission in multilayer dielectric structures'
-    by Celestino Creatore and Lucio Claudio Andreani.
-
+Thin film calculations.
 """
 
 import matplotlib.pyplot as plt
-from lifetmm.Methods.SpontaneousEmissionRate import *
+import numpy as np
+from lifetmm.Methods.SpontaneousEmissionRate import LifetimeTmm
+SAVE = True  # Save figs and data? (bool)
 
 
-def medium():
-    """ Silicon to air semi-infinite half spaces.
-    """
-    # Create structure
-    st = LifetimeTmm()
-    st.add_layer(1550, 3)
-    st.add_layer(1550, 1.55)
-
-    # Set vacuum wavelength
+def t2_spe_vs_z():
+    # Vacuum wavelength
     lam0 = 1550
-    st.set_wavelength(lam0)
 
-    # Calculate spontaneous emission over whole structure
-    result = st.spe_structure()
-    z = result['z']
-    spe_rates = result['spe_rates']
-    spe_TE = spe_rates['spe_TE']
-    spe_TM_p = spe_rates['spe_TM_p']
-    spe_TM_s = spe_rates['spe_TM_s']
+    # Create plot
+    f, ax = plt.subplots(figsize=(15, 7))
 
-    # Plot spontaneous emission rates
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax1.plot(z, spe_TE, label='TE')
-    ax1.plot(z, spe_TM_p, label='TM')
-    ax1.plot(z, spe_TE+spe_TM_p, 'k', label='TE + TM')
-    ax2 = fig.add_subplot(212)
-    ax2.plot(z, spe_TM_s, label='TM')
+    n_list = [1.2, 1.5, 2]
+    for n in n_list:
+        print('Evaluating n={:g}'.format(n))
+        # Create structure
+        st = LifetimeTmm()
+        st.set_wavelength(lam0)
+        st.add_layer(4000, 1.56)  # 1.45 is silica glass substrate
+        st.add_layer(980, 1.56)
+        st.add_layer(4000, n)
 
-    # Plot layer boundaries
+        # Calculate spontaneous emission over whole structure
+        result = st.spe_structure()
+        z = result['z']
+        spe = result['spe']['total']
+        # Plot
+        ax.plot(z, spe, label=n, lw=2)
+        ax.axhline(y=n, xmin=0.8, xmax=1, ls='dotted', color='k', lw=2)
+
+    # Plot internal layer boundaries
     for z in st.get_layer_boundaries()[:-1]:
-        ax1.axvline(z, color='r', lw=2)
-        ax2.axvline(z, color='r', lw=2)
+        ax.axvline(z, color='k', lw=2)
 
-    ax1.set_title('Spontaneous Emission Rate. LHS n=3, RHS n=1.55.')
-    ax1.set_ylabel('$\Gamma / \Gamma_0$')
-    ax2.set_ylabel('$\Gamma /\Gamma_0$')
-    ax2.set_xlabel('z/$\lambda$')
-    ax1.legend(title='Horizontal Dipoles')
-    ax2.legend(title='Vertical Dipoles')
-    plt.savefig('../Images/SPE_er_n=3.png', dpi=300)
+    ax.axhline(1.56, ls='--', color='k', lw=2)
+    ax.set_title('Spontaneous emission rate at boundary for semi-infinite media. LHS n=1.57.')
+    ax.set_ylabel('$\Gamma / \Gamma_0$')
+    ax.set_xlabel('Position z ($\lambda$/2$\pi$)')
+    plt.legend()
+    plt.tight_layout()
+    if SAVE:
+        plt.savefig('../Images/spe_vs_z_t2.png', dpi=300)
     plt.show()
 
 
+def t2_spe_vs_n():
+    # Vacuum wavelength
+    lam0 = 1550
+
+    # n_list = np.linspace(1, 1.47, 20)
+    n_list = [1, 1.33, 1.37, 1.47]
+    spe_list = []
+    for n in n_list:
+        print('Evaluating n={:g}'.format(n))
+        # Create structure
+        st = LifetimeTmm()
+        st.set_wavelength(lam0)
+        # st.add_layer(0, 1.45)
+        st.add_layer(0, 1.56)
+        st.add_layer(980, 1.56)
+        st.add_layer(0, n)
+        # Calculate spontaneous emission of layer 0 (1st)
+        result = st.spe_layer(layer=1, emission='Lower', th_num=13)
+        spe = result['spe']['total']
+        result = st.spe_layer(layer=1, emission='Upper', th_num=13)
+        spe += result['spe']['total']
+        # Take average
+        spe /= 2
+        spe = np.mean(spe)
+        # Normalise to bulk (so that spe = 1 in the doped layer)
+        spe -= 0.5
+        # Append to list
+        spe_list.append(spe)
+
+    # Convert lists to arrays
+    n_list = np.array(n_list)
+    spe_list = np.array(spe_list)
+
+    # Plot
+    f, ax = plt.subplots(figsize=(15, 7))
+    ax.plot(n_list, spe_list, '.-')
+    ax.set_title('Decay rate of T2.')
+    ax.set_ylabel('$\Gamma / \Gamma_0$')
+    ax.set_xlabel('n')
+    plt.tight_layout()
+    if SAVE:
+        plt.savefig('../Images/spe_vs_n_t2_nosubstrate.png', dpi=300)
+        np.savez('../Data/spe_vs_n_t2_nosubstrate', n=n_list, spe=spe_list)
+    plt.show()
+
+
+def load_data():
+    data = np.load('./lifetmm/Data/spe_vs_n.npz')
+    print(data._files)
+
+
 if __name__ == "__main__":
-    medium()
+    t2_spe_vs_z()
+    # t2_spe_vs_n()
--- lifetmm/Examples/polman.py
+++ lifetmm/Examples/polman.py
@@ -9,16 +9,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import pi
 from lifetmm.Methods.SpontaneousEmissionRate import LifetimeTmm
-SAVE = True  # Save figs? (bool)
+SAVE = False  # Save figs and data? (bool)
 
 
 def fig3():
-    """ Plot the average decay rate of layer(normalised to bulk n) vs n of semi-infinite half space.
+    """ Plot the average decay rate of layer(normalised to bulk n) vs n of semi-infinite
+     half space.
+
+     Note:
+         * we use the spe_layer function as we only care about the Er-doped layer.
+         * th_num option is specified to give a higher accuracy on the integration.
     """
     # Vacuum wavelength
     lam0 = 1550
 
-    n_list = np.linspace(1, 2, 50)
+    n_list = np.linspace(1, 2, 10)
     spe_list = []
     for n in n_list:
         print('Evaluating n={:g}'.format(n))
@@ -28,14 +33,15 @@ def fig3():
         st.add_layer(1550, 1.5)
         st.add_layer(0, n)
         # Calculate spontaneous emission of layer 0 (1st)
-        result = st.spe_layer(layer=0, emission='Lower')
+        result = st.spe_layer(layer=0, emission='Lower', th_num=13)
         spe = result['spe']['total']
-        result = st.spe_layer(layer=0, emission='Upper')
+        result = st.spe_layer(layer=0, emission='Upper', th_num=13)
         spe += result['spe']['total']
         # Take average
         spe /= 2
+        spe = np.mean(spe)
         # Normalise to bulk
-        spe = np.mean(spe) - 1.5
+        spe -= 1.5
         # Append to list
         spe_list.append(spe)
     spe_list = np.array(spe_list)
@@ -43,10 +49,10 @@ def fig3():
     # Plot
     f, ax = plt.subplots(figsize=(15, 7))
     ax.plot(n_list, spe_list)
-    ax.set_title('Average spontaneous emission rate over doped layer (d=1550nm) compared to bulk.')
+    ax.set_title('Average spontaneous emission rate over doped layer (d=1550nm) '
+                 'compared to bulk.')
     ax.set_ylabel('$\Gamma / \Gamma_1.5$')
     ax.set_xlabel('n')
-    plt.legend()
     plt.tight_layout()
     if SAVE:
         plt.savefig('../Images/spe_vs_n.png', dpi=300)
@@ -55,52 +61,43 @@ def fig3():
 
 
 def fig4():
-    """ Silicon to air semi-infinite half spaces.
+    """ n=3 or n=1 (air) to n=1.5 (Erbium deopsition) semi-infinite half spaces.
+    Plot the average otal spontaneous emission rate of dipoles as a function of
+    distance from the interface.
     """
     # Vacuum wavelength
     lam0 = 1550
     # Plotting units
     units = lam0 / (2 * pi)
-    # Create structure 1
-    st = LifetimeTmm()
-    st.set_wavelength(lam0)
-    st.add_layer(4*units, 3)
-    st.add_layer(4*units, 1.5)
-    st.info()
-    # Calculate spontaneous emission over whole structure
-    result = st.spe_structure()
-    z = result['z']
-    # Shift so centre of structure at z=0
-    z -= st.get_structure_thickness() / 2
-    spe = result['spe']
 
-    # Plot spontaneous emission rates
+    # Create plot
     f, ax = plt.subplots(figsize=(15, 7))
-    ax.plot(z/units, spe['total'], label='n=3', lw=2)
 
-    # Create structure 2
-    st = LifetimeTmm()
-    st.set_wavelength(lam0)
-    st.add_layer(4*units, 1)
-    st.add_layer(4*units, 1.5)
-    st.info()
-    # Calculate spontaneous emission over whole structure
-    result = st.spe_structure()
-    z = result['z']
-    # Shift so centre of structure at z=0
-    z -= st.get_structure_thickness() / 2
-    spe = result['spe']
-    # Plot spontaneous emission rates
-    ax.plot(z/units, spe['total'], label='n=1', lw=2)
-
-    # Plot internal layer boundaries
-    for z in st.get_layer_boundaries()[:-1]:
+    n_list = [1, 3]
+    for n in n_list:
+        print('Evaluating n={:g}'.format(n))
+        # Create structure
+        st = LifetimeTmm()
+        st.set_wavelength(lam0)
+        st.add_layer(4*units, n)
+        st.add_layer(4*units, 1.5)
+        st.info()
+        # Calculate spontaneous emission over whole structure
+        result = st.spe_structure()
+        z = result['z']
         # Shift so centre of structure at z=0
         z -= st.get_structure_thickness() / 2
-        ax.axvline(z/units, color='k', lw=2)
+        spe = result['spe']['total']
+        # Plot spontaneous emission rates
+        ax.plot(z/units, spe, label=('n='+str(n)), lw=2)
+        ax.axhline(y=n, xmin=0, xmax=0.4, ls='dotted', color='k', lw=2)
 
-    ax.axhline(y=1, xmin=0, xmax=0.4, ls='dotted', color='k', lw=2)
-    ax.axhline(y=3, xmin=0, xmax=0.4, ls='dotted', color='k', lw=2)
+        # Plot internal layer boundaries
+        for z in st.get_layer_boundaries()[:-1]:
+            # Shift so centre of structure at z=0
+            z -= st.get_structure_thickness() / 2
+            ax.axvline(z/units, color='k', lw=2)
+
     ax.axhline(1.5, ls='--', color='k', lw=2)
     ax.set_title('Spontaneous emission rate at boundary for semi-infinite media. RHS n=1.5.')
     ax.set_ylabel('$\Gamma / \Gamma_0$')
@@ -113,5 +110,5 @@ def fig4():
 
 
 if __name__ == "__main__":
-    fig3()
-    # fig4()
+    # fig3()
+    fig4()
