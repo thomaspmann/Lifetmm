guided mode spe shapes seem ok. need to fix weightings. potential numerical error with one of the TM mode

lifetmm/Methods/SpontaneousEmissionRate.py
@@ -2,7 +2,7 @@ import time
 
 import numpy as np
 import scipy.integrate as integrate
-from numpy import pi, sin, sum, exp, sinc
+from numpy import pi, sin, sum, exp, sinc, conj
 from scipy.constants import c
 from tqdm import *
 
@@ -11,7 +11,7 @@ from lifetmm.Methods.TransferMatrix import TransferMatrix
 
 class LifetimeTmm(TransferMatrix):
 
-    def spe_layer_radiative(self, layer, emission='Lower', th_pow=8):
+    def calc_spe_layer_radiative(self, layer, emission='Lower', th_pow=8):
         """ Evaluate the spontaneous emission rates for dipoles in a layer radiating into 'Lower' or 'Upper' modes.
         Rates are normalised w.r.t. free space emission or a randomly orientated dipole.
         """
@@ -19,7 +19,8 @@ class LifetimeTmm(TransferMatrix):
         assert emission in ['Lower', 'Upper'], \
             ValueError('Emission option must be either "Upper" or "Lower".')
         assert isinstance(th_pow, int), ValueError('pow must be an integer.')
-
+        assert self.d_list[layer] != 0, ValueError('The layer must have a thickness to use this function.)')
+        assert self.guided is False, ValueError('self.guided must be false to fun this function.')
         # Flip the structure and solve using lower radiative equations for upper radiative modes.
         # Results are flipped back at the end of this function to give the correct orientation again.
         if emission == 'Upper':
@@ -27,7 +28,7 @@ class LifetimeTmm(TransferMatrix):
             layer = self.num_layers - layer - 1
 
         # Lower cladding incoming wave vector magnitude
-        k0 = self.k(0)
+        k0 = self.calc_k(0)
 
         # z positions to evaluate E at
         z = np.arange((self.z_step / 2.0), self.d_list[layer], self.z_step)
@@ -75,7 +76,7 @@ class LifetimeTmm(TransferMatrix):
             self.set_incident_angle(theta)
 
             # Wave vector components in layer (q, k_11 are angle dependent)
-            k, q, k_11 = self.wave_vector(layer)
+            k, q, k_11 = self.calc_wave_vector_components(layer)
 
             # Check that the mode exists
             assert k_11**2 < k0**2, ValueError('k_11 can not be larger than k0!')
@@ -85,7 +86,7 @@ class LifetimeTmm(TransferMatrix):
             # Calculate E field within layer
             self.set_field('E')
             # E field coefficients in terms of incoming amplitude
-            E_plus, E_minus = self.amplitude_coefficients(layer)
+            E_plus, E_minus = self.calc_layer_radiative_amplitudes(layer)
             E['TE'] = E_plus * exp(1j * q * z) + E_minus * exp(-1j * q * z)
             # Orthonormality condition: Normalise outgoing TE wave to medium refractive index.
             E['TE'] /= self.n_list[0]
@@ -95,7 +96,7 @@ class LifetimeTmm(TransferMatrix):
             # Calculate H field within layer
             self.set_field('H')
             # H field coefficients in terms of incoming amplitude
-            H_plus, H_minus = self.amplitude_coefficients(layer)
+            H_plus, H_minus = self.calc_layer_radiative_amplitudes(layer)
             # Calculate the electric field component perpendicular (s) to the interface
             E['TM_s'] = k_11*(H_plus * exp(1j * q * z) + H_minus * exp(-1j * q * z))
             # Calculate the electric field component parallel (p) to the interface
@@ -111,7 +112,7 @@ class LifetimeTmm(TransferMatrix):
                 E[key] = abs(E[key]) ** 2 * sin(theta)
 
             # Wave vector components in upper cladding
-            q_clad = self.q(self.num_layers-1)
+            q_clad = self.calc_q(self.num_layers - 1)
             # Split solutions into partial and fully radiative modes
             if np.iscomplex(q_clad):
                 E2_z_th['TE_partial'][i, :] += E['TE'].real
@@ -199,7 +200,7 @@ class LifetimeTmm(TransferMatrix):
             ind = np.where(z_mat == layer)
 
             # Calculate lower radiative modes
-            spe_layer = self.spe_layer_radiative(layer, emission='Lower')['spe']
+            spe_layer = self.calc_spe_layer_radiative(layer, emission='Lower')['spe']
             spe['TE_lower'][ind] += spe_layer['TE']
             spe['TM_p_lower'][ind] += spe_layer['TM_p']
             spe['TM_s_lower'][ind] += spe_layer['TM_s']
@@ -211,7 +212,7 @@ class LifetimeTmm(TransferMatrix):
             spe['TM_p_lower_partial'][ind] += spe_layer['TM_p_partial']
 
             # Calculate upper radiative modes (always radiative as n[0] > n[-1])
-            spe_layer = self.spe_layer_radiative(layer, emission='Upper')['spe']
+            spe_layer = self.calc_spe_layer_radiative(layer, emission='Upper')['spe']
             spe['TE_upper'][ind] += spe_layer['TE']
             spe['TM_s_upper'][ind] += spe_layer['TM_s']
             spe['TM_p_upper'][ind] += spe_layer['TM_p']
@@ -227,13 +228,23 @@ class LifetimeTmm(TransferMatrix):
 
         return {'z': z_pos, 'spe': spe}
 
-    def spe_layer_guided(self, layer):
+    def calc_spe_layer_guided(self, layer):
         self.guided = True
-        # Evaluate guiding layer in structure(one with highest refractive index)
-        n = self.n_list
-        layer_guiding = np.where(n == max(n))[0][0]
+
+        # # Evaluate all layer and cladding field coefficients for guiding field
+        # amp_plus = np.zeros(self.num_layers, dtype=complex)
+        # amp_minus = np.zeros(self.num_layers, dtype=complex)
+        # for j in range(0, self.num_layers):
+        #     plus, minus = self.calc_layer_guided_amplitudes(j)
+        #     amp_plus[j] = plus
+        #     amp_minus[j] = minus
+
+        # # Evaluate guiding layer in structure(one with highest refractive index)
+        # n = self.n_list
+        # layer_guiding = np.where(n == max(n))[0][0]
 
         # z positions to evaluate E at
+        assert self.d_list[layer] != 0, ValueError('The layer must have a thickness to use this function.)')
         z = np.arange((self.z_step / 2.0), self.d_list[layer], self.z_step)
         if layer == 0:
             # A_plus and A_minus are defined at first cladding-layer boundary.
@@ -241,44 +252,57 @@ class LifetimeTmm(TransferMatrix):
             z = -z[::-1]
 
         # Structure to hold field E(z) components of each mode for each dipole orientation for a given theta
-        E = np.zeros(len(z), dtype=[('TE', 'complex128'),
-                                    ('TM_p', 'complex128'),
-                                    ('TM_s', 'complex128')])
+        electric_field = np.zeros(len(z), dtype=[('TE', 'complex128'),
+                                                 ('TM_p', 'complex128'),
+                                                 ('TM_s', 'complex128')])
 
         # Structure to hold field SPE(z) components of each mode for each dipole orientation for a theta
         spe = np.zeros(len(z), dtype=[('total', 'float64'),
                                       ('TE', 'float64'),
                                       ('TM_p', 'float64'),
                                       ('TM_s', 'float64')])
+        spe1 = np.zeros(len(z))
 
         # !* TE guided modes *!
         self.set_polarization('TE')
         # Calculate E field within layer
         self.set_field('E')
-        # Find guided modes parallel wave vector
-        for alpha in self.find_guided_modes():
-            self.n_11 = alpha
-            # Wave vector components in layer (q, k_11 are angle dependent)
-            k, q, k_11 = self.wave_vector(layer)
-            assert k_11 == (self.n_11*self.k(-1)), ValueError('Check TE')
+        for mode in self.calc_guided_modes():
+            self.n_11 = mode
 
-            # TODO: Find corresponding phase velocity
-            v = 1
+            # Evaluate the normalisation (B4) and apply
+            norm = 0
+            for j in range(0, self.num_layers):
+                k, q, k_11 = self.calc_wave_vector_components(j)
+                a, b = self.calc_layer_guided_amplitudes(j)
+                if j == 0:
+                    chi = np.imag(q)
+                    norm += abs(b) ** 2 * (chi ** 2 + k_11 ** 2) / (2 * chi)
+                elif j == self.num_layers - 1:
+                    chi = np.imag(q)
+                    norm += abs(a) ** 2 * (chi ** 2 + k_11 ** 2) / (2 * chi)
+                else:
+                    dj = self.d_list[j]
+                    w1 = (k_11 ** 2 + q * conj(q)) * sinc((q - conj(q)) * dj / 2)
+                    w2 = (k_11 ** 2 - q * conj(q)) * sinc((q + conj(q)) * dj / 2)
+                    norm += dj * (w1 * (abs(a) ** 2 + abs(b) ** 2) + w2 * (conj(a) * b + conj(b) * a))
+            norm = 1 / np.sqrt(np.real(norm))
 
-            # E field coefficients in terms of incoming amplitude
-            E_plus, E_minus = self.amplitude_coefficients(layer)
-            # Correct amplitudes with the orthonormality condition (B4)
-            chi_0 = self.q(0).imag
-            B_0 = self.amplitude_coefficients(0)
-            norm = (chi_0**2+k_11**2)/(2*chi_0)
+            # E field coefficients in terms of layer 0 (superstrate) outgoing field amplitude
+            a, b = self.calc_layer_guided_amplitudes(layer)
+            a *= norm
+            b *= norm
 
-            chi_m_plus_1 = self.q(self.num_layers - 1).real
+            # Wave vector components in layer
+            k, q, k_11 = self.calc_wave_vector_components(layer)
+            assert k_11 == (self.n_11 * self.calc_k(-1)), ValueError('Check TE')
 
             # Evaluate E(z)
-            E['TE'] = E_plus * exp(1j * q * z) + E_minus * exp(-1j * q * z)
-            assert max(E['TE']) < 100, ValueError('TMM likely unstable.')
-            E['TE'] = abs(E['TE']) ** 2 * (k / v)
-            spe['TE'] += E['TE']
+            electric_field['TE'] = a * exp(1j * q * z) + b * exp(-1j * q * z)
+            assert max(electric_field['TE']) < 100, ValueError('TMM likely unstable.')
+            # TODO: Find corresponding phase velocity
+            v = c
+            spe['TE'] += abs(electric_field['TE']) ** 2 * (k / v)
         # Normalise emission rates to vacuum emission rate of a randomly orientated dipole
         spe['TE'] *= 3*pi*c/4
 
@@ -287,36 +311,74 @@ class LifetimeTmm(TransferMatrix):
         # Calculate H field within layer
         self.set_field('H')
         # Find guided modes parallel wave vector
-        for alpha in self.find_guided_modes():
+        for alpha in self.calc_guided_modes():
             self.n_11 = alpha
+
+            # Evaluate the normalisation (B8) and apply
+            norm = 0
+            for j in range(0, self.num_layers):
+                k, q, k_11 = self.calc_wave_vector_components(j)
+                a, b = self.calc_layer_guided_amplitudes(j)
+                if j == 0:
+                    chi = np.imag(q)
+                    norm += abs(b) ** 2 / (2 * chi)
+                elif j == self.num_layers - 1:
+                    chi = np.imag(q)
+                    norm += abs(a) ** 2 / (2 * chi)
+                else:
+                    dj = self.d_list[j]
+                    w1 = sinc((q - conj(q)) * dj / 2)
+                    w2 = sinc((q + conj(q)) * dj / 2)
+                    norm += dj * (w1 * (abs(a) ** 2 + abs(b) ** 2) + w2 * (conj(a) * b + conj(b) * a))
+            norm = 1 / np.sqrt(np.real(norm))
+
+            # E field coefficients in terms of layer 0 (superstrate) outgoing field amplitude
+            a, b = self.calc_layer_guided_amplitudes(layer)
+            a *= norm
+            b *= norm
+
             # Wave vector components in layer (q, k_11 are angle dependent)
-            k, q, k_11 = self.wave_vector(layer)
-            assert k_11 == (self.n_11*self.k(-1)), ValueError('Check TM')
+            k, q, k_11 = self.calc_wave_vector_components(layer)
+            assert k_11 == (self.n_11 * self.calc_k(-1)), ValueError('Check TM')
+
+            # Calculate the electric field component perpendicular (s) and parallel (p) to the interface
+            coeff = 1j / self.n_list[layer]**2
+            electric_field['TM_s'] = coeff * (a * exp(1j * q * z) + b * exp(-1j * q * z))
+            electric_field['TM_p'] = coeff * (a * exp(1j * q * z) - b * exp(-1j * q * z))
+            if layer != 0 or layer != self.num_layers - 1:
+                electric_field['TM_s'] *= k_11
+                electric_field['TM_p'] *= q
+            assert max(electric_field['TM_s']) < 100, ValueError('TMM Unstable.')
+            assert max(electric_field['TM_p']) < 100, ValueError('TMM Unstable.')
 
             # TODO: Find corresponding phase velocity
             v = 1
 
-            # H field coefficients in terms of incoming amplitude
-            H_plus, H_minus = self.amplitude_coefficients(layer)
-            # Calculate the electric field component perpendicular (s) to the interface
-            E['TM_s'] = k_11 * (H_plus * exp(1j * q * z) + H_minus * exp(-1j * q * z))
-            # Calculate the electric field component parallel (p) to the interface
-            E['TM_p'] = q * (H_plus * exp(1j * q * z) - H_minus * exp(-1j * q * z))
-            assert max(E['TM_s']) < 100, ValueError('TMM Unstable.')
-            assert max(E['TM_p']) < 100, ValueError('TMM Unstable.')
-            E['TM_s'] = abs(E['TM_s']) ** 2 * (k / v)
-            E['TM_p'] = abs(E['TM_p']) ** 2 * (k / v)
-            spe['TM_s'] += E['TM_s']
-            spe['TM_p'] += E['TM_p']
-        # Normalise emission rates to vacuum emission rate of a randomly orientated dipole
-        # TODO: omega
-        omega = 1
-        spe['TM_s'] *= (3*pi*c**5)/(4*omega**4)
-        spe['TM_p'] *= (3*pi*c**5)/(2*omega**4)
+            spe['TM_s'] += abs(electric_field['TM_s']) ** 2 * (k / v)
+            spe['TM_p'] += abs(electric_field['TM_p']) ** 2 * (k / v)
+        # # Normalise emission rates to vacuum emission rate of a randomly orientated dipole
+        # # TODO: omega
+        # omega = 1
+        # spe['TM_s'] *= (3*pi*c**5)/(4*omega**4)
+        # spe['TM_p'] *= (3*pi*c**5)/(2*omega**4)
 
-        return {'z': z, 'E': E}
+        return {'z': z, 'spe': spe}
 
-    def spe_structure_guided(self):
+    def calc_spe_structure_guided(self):
+        # z positions to evaluate E field at over entire structure
+        z_pos = np.arange((self.z_step / 2.0), self.d_cumulative[-1], self.z_step)
+
+        # get z_mat - specifies what layer the corresponding point in z_pos is in
+        comp1 = np.kron(np.ones((self.num_layers, 1)), z_pos)
+        comp2 = np.transpose(np.kron(np.ones((len(z_pos), 1)), self.d_cumulative))
+        z_mat = sum(comp1 > comp2, 0)
+
+        # Structure to hold field spontaneous emission rate components over z
+        spe = np.zeros(len(z_pos), dtype=[('total', 'float64'),
+                                          ('TE', 'float64'),
+                                          ('TM_p', 'float64'),
+                                          ('TM_s', 'float64')])
+
         # Calculate emission rates for guided modes in each layer
         print('Evaluating lower and upper radiative modes for each layer:')
         for layer in range(self.num_layers):
@@ -328,6 +390,20 @@ class LifetimeTmm(TransferMatrix):
             else:
                 print('\tLayer -> internal {0:d} / {1:d}...'.format(layer, self.num_layers-2))
             time.sleep(0.2)  # Fixes progress bar occurring before text
+
+            # Find indices corresponding to the layer we are evaluating
+            ind = np.where(z_mat == layer)
+
+            # Calculate lower radiative modes
+            spe_layer = self.calc_spe_layer_guided(layer)['spe']
+            spe['TE'][ind] += spe_layer['TE']
+            spe['TM_p'][ind] += spe_layer['TM_p']
+            spe['TM_s'][ind] += spe_layer['TM_s']
+
+        # Totals
+        spe['total'] = spe['TE'] + spe['TM_p'] + spe['TM_s']
+
+        return {'z': z_pos, 'spe': spe}
 
 
 # Helper Functions

lifetmm/Methods/TransferMatrix.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy as sp
 from numpy import pi, sqrt, sin, exp
 from numpy.linalg import det
 
@@ -78,7 +77,7 @@ class TransferMatrix:
                              'inputs for thicknesses and wavelengths for greater resolution ')
         self.z_step = step
 
-    def k(self, j):
+    def calc_k(self, j):
         """ Calculate the wave vector magnitude in layer j. Alternatively if j ==- 1
         then we calculate the vacuum wave vector (k=omega/c).
         """
@@ -88,7 +87,7 @@ class TransferMatrix:
             n = self.n_list[j].real
         return 2 * pi * n / self.lam_vac
 
-    def xi(self, j):
+    def calc_xi(self, j):
         """ Normalised perpendicular wave-vector in layer j.
         """
         # Normalised wave-vector in layer
@@ -98,45 +97,45 @@ class TransferMatrix:
         # and medium for incoming wave (hence radiative mode)
         if not self.guided:
             n0 = self.n_list[0].real
-            n_11 = n0*sin(self.th)
+            n_11 = n0 * sin(self.th)
         else:
             n_11 = self.n_11
-        return sqrt(nj**2 - n_11**2)
+        return sqrt(nj ** 2 - n_11 ** 2)
 
-    def q(self, j):
+    def calc_q(self, j):
         """ Perpendicular wave vector in layer j
         """
-        xi = self.xi(j)
-        k_vac = self.k(-1)
+        xi = self.calc_xi(j)
+        k_vac = self.calc_k(-1)
         return xi * k_vac
 
-    def k_11(self):
+    def calc_k_11(self):
         """ Parallel wave vector (same in all layers due to BCs)
         """
         if not self.guided:
-            k0 = self.k(0)
+            k0 = self.calc_k(0)
             k_11 = k0 * sin(self.th)  # Note th needs to be in same layer as k0
         else:
             # TODO: check - n_11 will only be at discrete values.
             # Probably multiply by k(j=guided instead)
-            k_11 = self.n_11 * self.k(-1)
+            k_11 = self.n_11 * self.calc_k(-1)
         return k_11
 
-    def wave_vector(self, j):
+    def calc_wave_vector_components(self, j):
         """ The wave vector magnitude and it's components perpendicular and parallel
-        to the interface inside the layer calculated from the incident angle of wave.
+        to the interface inside the layer j.
         """
         # Layer wave vector and components
-        k = self.k(j)
-        k_11 = self.k_11()
-        q = self.q(j)
+        k = self.calc_k(j)
+        k_11 = self.calc_k_11()
+        q = self.calc_q(j)
         return k, q, k_11
 
-    def i_matrix(self, j, k):
+    def calc_i_matrix(self, j, k):
         """ Returns the interference matrix between layers j and k.
         """
-        xj = self.xi(j)
-        xk = self.xi(k)
+        xj = self.calc_xi(j)
+        xk = self.calc_xi(k)
         nj = self.n_list[j]
         nk = self.n_list[k]
         # Evaluate reflection and transmission coefficients for E field
@@ -157,75 +156,95 @@ class TransferMatrix:
             t = np.nan
         return (1 / t) * np.array([[1, r], [r, 1]], dtype=complex)
 
-    def l_matrix(self, j):
+    def calc_l_matrix(self, j):
         """ Returns the propagation L matrix for layer j.
         """
-        qj = self.q(j)
+        qj = self.calc_q(j)
         dj = self.d_list[j]
-        assert -1j*qj*dj < 25, \
+        assert -1j * qj * dj < 25, \
             ValueError('L_matrix is unstable for such a large thickness with an exponentially growing mode.')
-        return np.array([[exp(-1j*qj*dj), 0], [0, exp(1j*qj*dj)]], dtype=complex)
+        return np.array([[exp(-1j * qj * dj), 0], [0, exp(1j * qj * dj)]], dtype=complex)
 
-    def s_matrix(self):
+    def calc_s_matrix(self):
         """ Returns the total system transfer matrix s.
         """
-        s = self.i_matrix(0, 1)
+        s = self.calc_i_matrix(0, 1)
         for j in range(1, self.num_layers - 1):
-            l = self.l_matrix(j)
-            i = self.i_matrix(j, j + 1)
+            l = self.calc_l_matrix(j)
+            i = self.calc_i_matrix(j, j + 1)
             s = s @ l @ i
         return s
 
-    def s_primed_matrix(self, layer):
+    def calc_s_primed_matrix(self, layer):
         """ Returns the partial system transfer matrix s_prime.
         """
-        s_prime = self.i_matrix(0, 1)
+        s_prime = self.calc_i_matrix(0, 1)
         for j in range(1, layer):
-            l = self.l_matrix(j)
-            i = self.i_matrix(j, j + 1)
+            l = self.calc_l_matrix(j)
+            i = self.calc_i_matrix(j, j + 1)
             s_prime = s_prime @ l @ i
         return s_prime
 
-    def s_dprimed_matrix(self, layer):
+    def calc_s_dprimed_matrix(self, layer):
         """ Returns the partial system transfer matrix s_dprime (doubled prime).
         """
-        s_dprime = self.i_matrix(layer, layer + 1)
+        s_dprime = self.calc_i_matrix(layer, layer + 1)
         for j in range(layer + 1, self.num_layers - 1):
-            l = self.l_matrix(j)
-            i = self.i_matrix(j, j + 1)
+            l = self.calc_l_matrix(j)
+            i = self.calc_i_matrix(j, j + 1)
             s_dprime = s_dprime @ l @ i
         return s_dprime
 
-    def amplitude_coefficients(self, layer):
+    def calc_layer_radiative_amplitudes(self, layer):
         """ Evaluate fwd and bkwd field amplitude coefficients (E or H) in a layer.
          Coefficients are in units of the fwd incoming wave amplitude.
         """
         # Transfer matrix of system
-        s = self.s_matrix()
+        s = self.calc_s_matrix()
         # Reflection for incoming wave incident of LHS of structure
         r = s[1, 0] / s[0, 0]
         # Evaluate lower cladding
         if layer == 0:
-            field_plus = 1
+            field_plus = 1 + 0j
             field_minus = r
         # Evaluate upper cladding
         elif layer == self.num_layers - 1:
             field_plus = 1 / s[0, 0]
-            field_minus = 0
+            field_minus = 0 + 0j
         # Evaluate field amplitudes in internal layers
         else:
-            s_prime = self.s_primed_matrix(layer)
+            s_prime = self.calc_s_primed_matrix(layer)
             field_plus = (s_prime[1, 1] - r * s_prime[0, 1]) / det(s_prime)
             field_minus = (r * s_prime[0, 0] - s_prime[1, 0]) / det(s_prime)
         return field_plus, field_minus
 
-    def layer_field(self, layer):
+    def calc_layer_guided_amplitudes(self, layer):
+        """ Evaluate fwd and bkwd field amplitude coefficients (E or H) in a layer for guided modes.
+         Coefficients are in units of the superstrate (j=1) outgoing wave amplitude.
+        """
+        # Evaluate lower cladding
+        if layer == 0:
+            field_plus = 0 + 0j
+            field_minus = 1 + 0j
+        # Evaluate upper cladding
+        elif layer == self.num_layers - 1:
+            s = self.calc_s_matrix()
+            field_plus = 1 / s[0, 1]
+            field_minus = 0 + 0j
+        # Evaluate field amplitudes in internal layers
+        else:
+            s_prime = self.calc_s_primed_matrix(layer)
+            field_plus = - s_prime[0, 1] / det(s_prime)
+            field_minus = s_prime[0, 0] / det(s_prime)
+        return np.real_if_close(field_plus), np.real_if_close(field_minus)
+
+    def calc_layer_field(self, layer):
         """ Evaluate the field (E or H) as a function of z (depth) into the layer, j.
         field_plus is the forward component of the field (e.g. E_j^+)
         field_minus is the backward component of the field (e.g. E_j^-)
         """
         # Wave vector components in layer
-        k, q, k_11 = self.wave_vector(layer)
+        k, q, k_11 = self.calc_wave_vector_components(layer)
 
         # z positions to evaluate field at at
         z = np.arange((self.z_step / 2.0), self.d_list[layer], self.z_step)
@@ -235,9 +254,9 @@ class TransferMatrix:
             z = -z[::-1]
 
         # field(z) field in terms of incident field amplitude (A_0^+)
-        field_plus, field_minus = self.amplitude_coefficients(layer)
+        field_plus, field_minus = self.calc_layer_radiative_amplitudes(layer)
         field = field_plus * exp(1j * q * z) + field_minus * exp(-1j * q * z)
-        field_squared = abs(field)**2
+        field_squared = abs(field) ** 2
         if self.d_list[layer] != 0:
             field_avg = sum(field_squared) / (self.z_step * self.d_list[layer])
         else:
@@ -257,13 +276,13 @@ class TransferMatrix:
         for layer in range(self.num_layers):
             # Calculate z indices inside structure for the layer
             z_indices = np.where(z_mat == layer)
-            field[z_indices] = self.layer_field(layer)['field']
-        field_squared = abs(field)**2
+            field[z_indices] = self.calc_layer_field(layer)['field']
+        field_squared = abs(field) ** 2
         return {'z': z, 'field': field, 'field_squared': field_squared}
 
-    def s11(self, n_11):
+    def _s11(self, n_11):
         self.n_11 = n_11
-        s = self.s_matrix()
+        s = self.calc_s_matrix()
         return s[0, 0].real
 
     def s11_guided(self):
@@ -276,17 +295,17 @@ class TransferMatrix:
         n_11_range = np.linspace(n[0], max(n), num=1000, endpoint=False)[1:]
         s_11 = np.array([])
         for n_11 in n_11_range:
-            s_11 = np.append(s_11, self.s11(n_11))
+            s_11 = np.append(s_11, self._s11(n_11))
         return n_11_range, s_11.real
 
-    def find_guided_modes(self):
+    def calc_guided_modes(self):
         """ Evaluate beta at S_11=0 as a function of k_ll in the guided regime.
         Guided regime:  n_clad < k_ll/k < max(n), k_11/k = n_11
         """
         assert self.guided, \
             ValueError('"self.guided" must be set to true before running this function.')
         n = self.n_list.real
-        alphas = roots(self.s11, n[0], max(n))
+        alphas = roots(self._s11, n[0], max(n))
         return alphas
 
     def show_structure(self):
@@ -331,18 +350,18 @@ class TransferMatrix:
         """
         return self.d_cumulative[-1]
 
-    def z_to_lambda(self, z, center=True):
+    def calc_z_to_lambda(self, z, center=True):
         """ Convert z positions to units of wavelength (optional) from the centre.
         """
         if center:
-            z -= self.get_structure_thickness()/2
+            z -= self.get_structure_thickness() / 2
         z /= self.lam_vac
         return z
 
     def calc_r_and_t(self):
         """ Return the complex reflection and transmission coefficients of the structure.
         """
-        s = self.s_matrix()
+        s = self.calc_s_matrix()
         r = s[1, 0] / s[0, 0]
         t = 1 / s[0, 0]
         return r, t
@@ -360,8 +379,8 @@ class TransferMatrix:
             n_2 = self.n_list[-1].real
             th_out = snell(n_1, n_2, self.th)
             rho = n_2 / n_1
-            m = np.cos(th_out)/np.cos(self.th)
-            transmission *= rho*m
+            m = np.cos(th_out) / np.cos(self.th)
+            transmission *= rho * m
         return reflection, transmission
 
     def calc_absorption(self):